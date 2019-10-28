5708fba8b57b4645e44549bc1e2a18d9a0bb1852
Vinz <vincentballut@outlook.fr>
2019-10-28 12:02:49 +0100

    get one todo

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,10 +1,17 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { Todo } from './interfaces/todo.interface';
 
 @Controller('todos') // equals localhost:3000/todos
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // console.log('id', id);
+    return this.todosService.findOne(id);
+  }
+
   @Get()
   findAll(): Todo[] {
     return this.todosService.findAll();
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -28,6 +28,10 @@ export class TodosService {
     return this.todos;
   }
 
+  findOne(id: string): Todo[] {
+    return this.todos.filter(todo => todo.id === Number(id));
+  }
+
   create(todo: Todo) {
     this.todos = [...this.todos, todo];
   }
